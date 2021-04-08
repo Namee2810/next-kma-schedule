a3aaf672f32fe0adf11596c01e8902b61cb28671
Namee2810 <dvn281002@gmail.com>
2021-04-08 22:28:18 +0700

    pwa fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
 import 'antd/dist/antd.css';
+import { useEffect } from 'react';
 import "../styles/calendar.css";
 import '../styles/globals.css';
 import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          (registration) => {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          (err) => {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, [])
   return <Component {...pageProps} />
 }
 
