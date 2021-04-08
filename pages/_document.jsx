30d229b7e5279ae5160bc8dac8b4f49b4b0dda88
Namee2810 <dvn281002@gmail.com>
2021-04-09 00:51:10 +0700

    seo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,6 +9,11 @@ class MyDocument extends Document {
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta http-equiv="Cotent-Type" content="text/html; charset=utf-8" />
           <meta name="description" content="Thời khóa biểu trực tuyến dành cho sinh viên Học viện Kỹ thuật Mật mã (KMA)" />
+
+          <meta property="og:url" content="https://kma.namee.site" key="ogurl" />
+          <meta property="og:image" content="https://kma.namee.site/images/vercel.png" key="ogimage" />
+          <meta property="og:title" content="Thời khóa biểu KMA" key="ogtitle" />
+          <meta property="og:description" content="Thời khóa biểu trực tuyến dành cho sinh viên Học viện Kỹ thuật Mật mã (KMA)" key="ogdesc" />
         </Head>
         <body>
           <Main />
