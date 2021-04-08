3dcc2361cbab924e54d15cfaaf3bca8c4a0f74b5
Namee2810 <dvn281002@gmail.com>
2021-04-08 22:07:05 +0700

    Update authToken.js

--- modules/authToken.js
+++ modules/authToken.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 function authToken(token) {
   let data;
-  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+  jwt.verify(token, process.env.NEXT_PUBLIC_SECRET_KEY, (err, decoded) => {
     data = decoded;
   });
   return data;
