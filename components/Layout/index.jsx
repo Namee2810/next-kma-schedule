794c546f436d20281d74f392c02a1d55333ba363
Namee2810 <dvn281002@gmail.com>
2021-04-27 20:24:29 +0700

    Revert "."
    This reverts commit cf66de6cb738f72eb4a1ac548528cb6d6ceca86c.

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,25 +1,9 @@
-import authToken from "modules/authToken";
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { SET_SCHEDULE, SET_STUDENT } from "store/reducer";
+import React from 'react';
 import Header from './Header';
 import Nav from './Nav';
 import styles from "./styles.module.scss";
 
 function Layout(props) {
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const local = localStorage.getItem("schedule");
-    if (!local) return router.push("/auth");
-    else {
-      const decoded = authToken(local);
-      dispatch(SET_STUDENT({ student: decoded.studentProfile }))
-      dispatch(SET_SCHEDULE({ schedule: decoded.schedule }))
-    }
-  }, [])
   return (
     <div className={styles.Layout}>
       <Header className={styles.Layout_header} />
