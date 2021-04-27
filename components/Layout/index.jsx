cf66de6cb738f72eb4a1ac548528cb6d6ceca86c
Namee2810 <dvn281002@gmail.com>
2021-04-27 20:20:17 +0700

    .

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import authToken from "modules/authToken";
+import { useRouter } from 'next/router';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { SET_SCHEDULE, SET_STUDENT } from "store/reducer";
 import Header from './Header';
 import Nav from './Nav';
 import styles from "./styles.module.scss";
 
 function Layout(props) {
+  const router = useRouter();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const local = localStorage.getItem("schedule");
+    if (!local) return router.push("/auth");
+    else {
+      const decoded = authToken(local);
+      dispatch(SET_STUDENT({ student: decoded.studentProfile }))
+      dispatch(SET_SCHEDULE({ schedule: decoded.schedule }))
+    }
+  }, [])
   return (
     <div className={styles.Layout}>
       <Header className={styles.Layout_header} />
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,8 @@
 import Schedule from "components/HomePage/Schedule";
 import Layout from "components/Layout";
-import authToken from "modules/authToken";
 import Head from 'next/head';
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { SET_SCHEDULE, SET_STUDENT } from "store/reducer";
 
 export default function Home() {
-  const router = useRouter();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const local = localStorage.getItem("schedule");
-    if (!local) return router.push("/auth");
-    else {
-      const decoded = authToken(local);
-      dispatch(SET_STUDENT({ student: decoded.studentProfile }))
-      dispatch(SET_SCHEDULE({ schedule: decoded.schedule }))
-    }
-  }, [])
 
   return (
     <>
