95a8b71d04934c765df31068c33fc1e76cef95de
Namee2810 <dvn281002@gmail.com>
2021-04-29 22:04:24 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import 'antd/dist/antd.css';
-import Head from 'next/head';
 import "../styles/calendar.css";
 import '../styles/globals.css';
 import "../styles/styles.scss";
 
 function MyApp({ Component, pageProps }) {
-  < Head >
-    <link
-      rel="preload"
-      href="/images/auth_bg.webp"
-      as="image"
-    />
-  </Head >
   return <Component {...pageProps} />
 }
 
