94b007c9013e2b7e241ccc4a3c49109f7d37a8f6
Namee2810 <dvn281002@gmail.com>
2021-07-27 11:14:10 +0700

    update nav

--- src/layouts/HomeLayout/Nav/index.jsx
+++ src/layouts/HomeLayout/Nav/index.jsx
@@ -4,35 +4,30 @@ import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
 import ActiveLink from 'components/common/ActiveLink';
 import { useRouter } from "next/router";
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { SIGN_OUT } from "store/reducer";
 import styles from "./styles.module.scss";
 
 function nav(props) {
   const router = useRouter();
   const dispatch = useDispatch();
-  const schedule = useSelector(state => state.schedule);
 
-  const navItems = [
-    {
-      title: "Thời khóa biểu",
-      icon: <EventAvailableOutlinedIcon style={{ fontSize: "34px" }} />,
-      href: "/"
-    },
-    {
-      title: "Thông tin sinh viên",
-      icon: <FaceIcon style={{ fontSize: "34px" }} />,
-      href: "/profile"
-    },
-    {
-      title: "Đăng xuất",
-      icon: <PowerSettingsNewIcon style={{ fontSize: "34px" }} />,
-      onClick() {
-        dispatch(SIGN_OUT())
-        router.push("/auth")
-      }
+  const navItems = [{
+    title: "Thời khóa biểu",
+    icon: <EventAvailableOutlinedIcon style={{ fontSize: "34px" }} />,
+    href: "/"
+  }, {
+    title: "Thông tin sinh viên",
+    icon: <FaceIcon style={{ fontSize: "34px" }} />,
+    href: "/profile"
+  }, {
+    title: "Đăng xuất",
+    icon: <PowerSettingsNewIcon style={{ fontSize: "34px" }} />,
+    onClick() {
+      dispatch(SIGN_OUT())
+      router.push("/auth")
     }
-  ]
+  }]
 
   return (
     <div className={styles.nav} id="nav">
